c3113a744570fc17bb96f00f392d035bbe731b8d
gauravkatoch <gauravkatoch007@gmail.com>
2016-01-03 00:05:53 +0530

    fixed static files issue

--- FinTech/urls.py
+++ FinTech/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -25,6 +26,7 @@ urlpatterns = [
     url(r'^getAvailableItems', 'FinTech.views.getAvailableItems'),
     url(r'^checkout', 'FinTech.views.checkout'),
     url(r'^payment', 'FinTech.views.payment'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 
 
 
